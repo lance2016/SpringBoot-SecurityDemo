dad6f9afd75629da6cd7acf5d1054cece5deec11
lance <lancefate@163.com>
2017-12-08 11:33:49 +0800

    基本的Spring boot +Spring security模板

--- src/main/java/com/lance/common/WebSecurityConfig.java
+++ src/main/java/com/lance/common/WebSecurityConfig.java
@@ -27,7 +27,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/home").permitAll()
+                .antMatchers("/index").permitAll()
                 //其他地址的访问均需验证权限
                 .anyRequest().authenticated()
                 .and()
